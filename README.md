5798cd8c34f6e8c6a2ad90453e23aa14747f5990
Ben Robbins <ben.robbins@gmail.com>
2015-02-28 12:46:56 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # brobbot-youtube
+
+A brobbot plugin for YouTube searches.
+
+```
+brobbot youtube [me] <query>
+```
+
+Searches YouTube for videos matching `query`, and returns a random result.
---
+++ index.js
@@ -0,0 +1,34 @@
+// Description:
+//   Search for a YouTube video
+
+module.exports = function(robot) {
+  robot.helpCommand("brobbot youtube [me] `query`", "Searches YouTube for `query` and returns the video link.");
+
+  robot.respond(/^youtube( me)? (.*)/i, function(msg) {
+    var query = msg.match[3];
+    robot.http("http://gdata.youtube.com/feeds/api/videos")
+      .query({
+        orderBy: "relevance",
+        'max-results': 15,
+        alt: 'json',
+        q: query
+      })
+      .get()(function(err, res, body) {
+        var videos = JSON.parse(body).feed.entry;
+
+        if (!videos) {
+          msg.send("No video results for '" + query + "'");
+          return;
+        }
+
+        var video = msg.random(videos);
+        console.log(video.link);
+        video.link.forEach(function(link) {
+          if (link.rel === "alternate" && link.type === "text/html") {
+            msg.send(link.href);
+            return false;
+          }
+        });
+      });
+  });
+};
