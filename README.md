7f1853632ca8a7a43dce62543a694982668a06c6
Ben Robbins <ben.robbins@gmail.com>
2015-06-04 07:56:39 -0500

    youtube api shutdown, use google video search api

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # brobbot-youtube
 
-A brobbot plugin for YouTube searches.
+A brobbot plugin for video searches.
 
 ```
 brobbot youtube [me] <query>
 ```
 
-Searches YouTube for videos matching `query`, and returns a random result.
+Searches Google for videos matching `query`, and returns a random result.
+
+## Configuration
+
+### Referer
+
+```bash
+BROBBOT_YOUTUBE_REFERER=url
+```
+
+The referer URL to pass to the Google API (see https://developers.google.com/video-search/v1/jsondevguide).
--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 // Description:
-//   Search for a YouTube video
+//   Search for a video
+//
+// Configuration:
+//   BROBBOT_YOUTUBE_REFERER - the referer URL to pass to the Google API (see https://developers.google.com/video-search/v1/jsondevguide)
 
 module.exports = function(robot) {
   robot.helpCommand("brobbot youtube [me] `query`", "Searches YouTube for `query` and returns the video link.");
 
+  var REFERER = process.env.BROBBOT_YOUTUBE_REFERER || 'https://npmjs.org/package/brobbot-youtube';
+
   robot.respond(/^youtube( me)? (.*)/i, function(msg) {
     var query = msg.match[2];
 
-    robot.http("http://gdata.youtube.com/feeds/api/videos")
+    msg.http('http://ajax.googleapis.com/ajax/services/search/video')
       .query({
         orderBy: "relevance",
         'max-results': 15,
-        alt: 'json',
+        responseFormat: 'json',
+        v: '1.0',
         q: query
       })
+      .header('Referer', REFERER)
       .get()(function(err, res, body) {
-        var videos = JSON.parse(body).feed.entry;
+        var videos = JSON.parse(body).responseData.results;
 
-        if (!videos) {
+        if (!videos || videos.length === 0) {
           msg.send("No video results for '" + query + "'");
           return;
         }
 
-        var video = msg.random(videos);
-        video.link.forEach(function(link) {
-          if (link.rel === "alternate" && link.type === "text/html") {
-            msg.send(link.href);
-            return false;
-          }
-        });
+        msg.send(msg.random(videos).url);
       });
   });
 };
